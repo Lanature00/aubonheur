a1689f23548e82a278862e2719c69db0d5ab6b00
Lanature00 <lanag27@free.fr>
2026-06-23 17:23:37 +0200

    Mise en place de l'architecture du projet
    Structure du projet
    Configuration Docker & PostgreSQL
    Backend Express.js
    Variables d'environnement (dotenv)
    Connexion Node.js ↔ PostgreSQL

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const pool = new Pool({
+  connectionString: `postgresql://admin:admin123@localhost:5432/aubonheur`,
+  ssl: false
+});
+
+pool.connect((err, client, release) => {
+  if (err) {
+    console.error('❌ Erreur de connexion à la base de données:', err.message);
+  } else {
+    console.log('✅ Connecté à PostgreSQL avec succès !');
+    release();
+  }
+});
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Le serveur fonctionne ! 🎉');
+});
+
+app.listen(PORT, () => {
+  console.log(`Serveur démarré sur http://localhost:${PORT}`);
+});
