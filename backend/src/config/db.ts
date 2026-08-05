eebcfd2966e090bb419e323614a9c69ab2b88ae9
Lanature00 <lanag27@free.fr>
2026-08-05 13:44:03 +0200

    fix: enable SSL for production database connection

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -5,7 +5,9 @@ dotenv.config();
 
 const pool = new Pool({
   connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
-  ssl: false
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 pool.connect((err, client, release) => {
